2cfa6a6c582c4c21c7281f9c051fb7a4d2bdb8fa
bhandarijyoti12 <jyotibhandarii32@outlook.com>
2022-04-27 12:34:47 -0400

    first commit

---
+++ webscraper.py
@@ -0,0 +1,31 @@
+# pip install selenium beautifulsoup4
+# imports
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+from time import sleep
+ 
+chromedriver_path = '/Users/jyotibhandari/Documents/Final_Project_AI/chromedriver.exe'
+service = Service('/Users/jyotibhandari/Documents/Final_Project_AI/chromedriver.exe')
+options = Options()
+options.add_argument("--headless")
+ 
+# function
+def scrape_page_text(url:str):
+    # create driver
+    driver = webdriver.Chrome(executable_path="/Users/jyotibhandari/Documents/Final_Project_AI/chromedriver",
+     options=options)
+    # launch driver
+    driver.get(url)
+    sleep(3)
+   
+    # get soup from driver page
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit()
+   
+    # scrape all the text from page
+    text = soup.get_text()
+    text = text.replace("\n", "")
+   
+    return(text)
